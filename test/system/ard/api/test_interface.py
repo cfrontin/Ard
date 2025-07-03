ad6286232f99be388a7960fe069d32ba201b2459
Jared Thomas <jaredthomas68@gmail.com>
2025-07-03 09:43:32 -0600

    move LCOE stack tests into api test directory

--- test/system/ard/api/test_interface.py
+++ test/system/ard/api/test_interface.py
@@ -7,15 +7,10 @@ from ard.api import set_up_ard_model, replace_key_value, set_up_system_recursive
 class TestSetUpArdModel:
     def setup_method(self):
         
-        input_dict = load_yaml(
-            Path(__file__).parent / "inputs" / "ard_system.yaml"
-        )
-        
-        self.prob = set_up_ard_model(input_dict=input_dict)
+        input_dict_path = str(Path(__file__).parent.absolute() / "inputs" / "ard_system.yaml")
+
+        self.prob = set_up_ard_model(input_dict=input_dict_path)
 
     def test_onshore_default_system_run_model(self):
 
-        try:
-            self.prob.run_model()
-        except:
-            assert False
+        self.prob.run_model()
