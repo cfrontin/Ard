6df62124d0087559e5eb59f73755946e16addeaa
Jared Thomas <jaredthomas68@gmail.com>
2025-08-26 13:52:38 -0600

    update 01

--- examples/01_onshore/optimization_demo.py
+++ examples/01_onshore/optimization_demo.py
@@ -42,7 +42,7 @@ def run_example():
     pp.pprint(test_data)
     print("\n\n")
 
-    optimize = False  # set to False to skip optimization
+    optimize = True  # set to False to skip optimization
 
     if optimize:
 
@@ -54,7 +54,7 @@ def run_example():
         test_data = {
             "AEP_val": float(prob.get_val("AEP_farm", units="GW*h")[0]),
             "CapEx_val": float(prob.get_val("tcc.tcc", units="MUSD")[0]),
-            "BOS_val": float(prob.get_val("total_capex", units="MUSD")[0]),
+            "BOS_val": float(prob.get_val("landbosse.total_capex", units="MUSD")[0]),
             "OpEx_val": float(prob.get_val("opex.opex", units="MUSD/yr")[0]),
             "LCOE_val": float(prob.get_val("financese.lcoe", units="USD/MW/h")[0]),
             "area_tight": float(prob.get_val("landuse.area_tight", units="km**2")[0]),
