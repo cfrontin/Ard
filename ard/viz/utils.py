d908e710ca7252c600978417b2150f4f0973e74a
Cory Frontin <cory.frontin@nrel.gov>
2025-10-28 09:56:09 -0600

    missed a bunch, trying again.

--- ard/viz/utils.py
+++ ard/viz/utils.py
@@ -5,6 +5,6 @@ def get_plot_range(values, pct_buffer=5.0):
     min_value = np.min(values)
     max_value = np.max(values)
     dvalues = max_value - min_value
-    min_value = min_value - pct_buffer/100.0*dvalues
-    max_value = max_value - pct_buffer/100.0*dvalues
+    min_value = min_value - pct_buffer / 100.0 * dvalues
+    max_value = max_value - pct_buffer / 100.0 * dvalues
     return min_value, max_value
