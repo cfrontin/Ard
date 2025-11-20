612a228d619024749c3fdca7be5bebc6d783d982
Cory Frontin <cory.frontin@nrel.gov>
2025-11-20 10:50:57 -0700

    fix bounds manager

--- ard/viz/utils.py
+++ ard/viz/utils.py
@@ -6,5 +6,5 @@ def get_plot_range(values, pct_buffer=5.0):
     max_value = np.max(values)
     dvalues = max_value - min_value
     min_value = min_value - pct_buffer / 100.0 * dvalues
-    max_value = max_value - pct_buffer / 100.0 * dvalues
+    max_value = max_value + pct_buffer / 100.0 * dvalues
     return min_value, max_value
