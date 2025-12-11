9fae2ab1428cd2837df95ece870149dc16bf55bf
Eliot Quon <eliot.quon@nrel.gov>
2025-12-10 22:07:38 -0700

    Fix parallel script

--- examples/x07_exclusion/run_parallel.sh
+++ examples/x07_exclusion/run_parallel.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 NPROCS=8
 
-alias generate_seeds=`seq 0 511`
-
-for fpath in inputs/exclusions/thresh_*.yaml; do
+for fpath in inputs/exclusions/*.yaml; do
 
     exclusions_yaml=${fpath##*/}
     work_dir=${exclusions_yaml%.yaml}
     prefix="${work_dir}/nonuniform"
     mkdir -p $work_dir
 
-    generate_seeds | parallel -j $NPROCS "echo 'Running opt prob {} with $exclusions_yaml'; ./optimization_sweep.py {} $exclusions_yaml &> ${prefix}{}.log"
+    seq 0 511 | parallel -j $NPROCS "echo 'Running opt prob {} with $exclusions_yaml'; ./optimization_sweep.py {} $exclusions_yaml &> ${prefix}{}.log"
 
 done
-
-work_dir='no_exclusion'
-prefix="${work_dir}/nonuniform"
-mkdir -p $work_dir
-generate_seeds | parallel -j $NPROCS "echo 'Running opt prob {} with NO exclusions'; ./optimization_sweep.py {} &> ${prefix}{}.log"
 
 ./check_full_sweep.sh
 ./check_floris_logs.sh
