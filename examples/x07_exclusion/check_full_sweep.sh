f3b7ec4ca50d929d49fa855435f3868d5bccd449
Eliot Quon <eliot.quon@nrel.gov>
2025-12-23 22:14:58 -0700

    Updated runscripts; warmstart is deprecated

--- examples/x07_exclusion/check_full_sweep.sh
+++ examples/x07_exclusion/check_full_sweep.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo 'Number of logs should match number of optimization statuses found...'
 
-for dname in thresh_0p*; do
+for dname in thresh_*; do
     nlog=`ls $dname/*.log | wc -l`
     nstat=`grep '^Optimization' $dname/*.log | wc -l`
     echo "$dname $nlog $nstat"
 done
 
-dname='no_exclusion'
-nlog=`ls $dname/*.log | wc -l`
-nstat=`grep '^Optimization' $dname/*.log | wc -l`
-echo "$dname $nlog $nstat"
+#dname='no_exclusion'
+#nlog=`ls $dname/*.log | wc -l`
+#nstat=`grep '^Optimization' $dname/*.log | wc -l`
+#echo "$dname $nlog $nstat"
--- examples/x07_exclusion/run_parallel.sh
+++ examples/x07_exclusion/run_parallel.sh
@@ -1,16 +1,61 @@
 #!/bin/bash
-NPROCS=8
+#SBATCH --job-name=ard
+#SBATCH --account=erf
+#SBATCH --time=8:00:00
+#SBATCH --ntasks=96
+#SBATCH -N 1
+#SBATCH --exclusive
+#SBATCH --qos=high
 
-for fpath in inputs/exclusions/*.yaml; do
+source ~/.bash_profile
+ard-env
 
-    exclusions_yaml=${fpath##*/}
-    work_dir=${exclusions_yaml%.yaml}
-    prefix="${work_dir}/nonuniform"
+# Macbook
+# -------
+NPROCS=8 # 0.83 min for 50 iters
+
+# Kestrel
+# -------
+# module purge
+# module load conda/2024.06.1
+# module load parallel/20220522
+# conda activate ard-env
+NPROCS=96 # 1.5 min for 50 iters
+
+prefix='nonuniform'
+starts=`seq 0 1023`
+threshold_values='0.25 0.26 0.27 0.28 0.29 0.3 0.31 0.32 0.33 0.34 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.8 0.9 1'  # 21
+
+run_opt() {
+    probnum=$1
+    thresh=$2
+
+    work_dir="thresh_${thresh}"
     mkdir -p $work_dir
 
-    seq 0 511 | parallel -j $NPROCS "echo 'Running opt prob {} with $exclusions_yaml'; ./optimization_sweep.py {} $exclusions_yaml &> ${prefix}{}.log"
+    prefix="${work_dir}/nonuniform"
+    outfile="${prefix}${probnum}.log"
 
-done
+    if grep '^Optimization' $outfile &> /dev/null; then
+        echo "Skipping opt prob $probnum with thresh=$thresh"
+    else
+        if [ -f "$outfile" ]; then
+            echo "Re-running opt prob $probnum with thresh=$thresh"
+            mv $outfile $outfile.last
+        else
+            echo "Running opt prob $probnum with thresh=$thresh"
+        fi
+
+        ./optimization_sweep_with_eco_constraint.py $probnum $thresh &> $outfile
+    fi
+}
+export -f run_opt  # make available to subshells spawned by gnu parallel
+
+echo "Starting optimization sweep at `date`"
+
+parallel -j $NPROCS run_opt ::: $starts ::: $threshold_values
+
+echo "Finished optimization sweep at `date`"
 
 ./check_full_sweep.sh
 ./check_floris_logs.sh
