56a5749c824270e031a1fa6f91bdb5c5213d7515
Cory Frontin <cory.frontin@nrel.gov>
2026-01-20 22:17:54 -0700

    add tools test but it doesn't do anything and errors out ha

---
+++ test/flowers/unit/test_tools.py
@@ -0,0 +1,45 @@
+# from pathlib import Path
+
+import pytest
+
+
+class TestToolsLayout:
+    def setup_method(self):
+        pass
+
+    def test_random_layut(self, subtests):
+        assert False
+
+    def test_discrete_layut(self, subtests):
+        assert False
+
+    def test_load_layut(self, subtests):
+        assert False
+
+
+class TestToolsWindRose:
+    def setup_method(self):
+        pass
+
+    def test_load_wind_rose(self, subtests):
+        assert False
+
+    def test_resample_wind_direction(self, subtests):
+        assert False
+
+    def test_resample_wind_speed(self, subtests):
+        assert False
+
+    def test_resample_average_ws_by_wd(self, subtests):
+        assert False
+
+
+class TestToolsLookup:
+    def setup_method(self):
+        pass
+
+    def test_ct_lookup(self, subtests):
+        assert False
+
+    def test_cp_lookup(self, subtests):
+        assert False
